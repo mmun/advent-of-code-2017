49612a70dcc09f0ba8c117513b88bbb99b6a6f7a
Martin Muñoz <im.mmun@gmail.com>
2017-12-02 02:59:29 -0500

    Day 2

---
+++ src/bin/2-1.rs
@@ -0,0 +1,32 @@
+fn solve(input: &str) -> i64 {
+    let input = input.trim();
+
+    let mut sum = 0;
+
+    for line in input.lines() {
+        let nums = line.split_whitespace()
+            .map(|word| word.parse().unwrap())
+            .collect::<Vec<i64>>();
+
+        let max = nums.iter().max().unwrap();
+        let min = nums.iter().min().unwrap();
+
+        sum += max - min;
+    }
+
+    sum
+}
+
+fn main() {
+    println!("{}", solve(include_str!("2.in")));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::solve;
+
+    #[test]
+    fn examples() {
+        assert_eq!(solve("5 1 9 5\n7 5 3\n2 4 6 8\n"), 18);
+    }
+}
---
+++ src/bin/2-2.rs
@@ -0,0 +1,37 @@
+fn solve(input: &str) -> i64 {
+    let input = input.trim();
+
+    let mut sum = 0;
+
+    for line in input.lines() {
+        let mut nums = line.split_whitespace()
+            .map(|word| word.parse().unwrap())
+            .collect::<Vec<i64>>();
+
+        nums.sort();
+
+        for i in 0..nums.len()-1 {
+            for j in i+1..nums.len() {
+                if nums[j] % nums[i] == 0 {
+                    sum += nums[j] / nums[i];
+                }
+            }
+        }
+    }
+
+    sum
+}
+
+fn main() {
+    println!("{}", solve(include_str!("2.in")));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::solve;
+
+    #[test]
+    fn examples() {
+        assert_eq!(solve("5 9 2 8\n9 4 7 3\n3 8 6 5\n"), 9);
+    }
+}
