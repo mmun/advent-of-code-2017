4479f415080266fce3dd957295bb59be5a11cbe6
Martin Muñoz <im.mmun@gmail.com>
2017-12-06 02:59:43 -0500

    Day 5

---
+++ src/bin/5-2.rs
@@ -0,0 +1,45 @@
+fn solve(input: &str) -> i64 {
+    let mut jumps = input
+        .trim()
+        .lines()
+        .map(|line| line.parse().unwrap())
+        .collect::<Vec<i64>>();
+
+    let mut count = 0;
+    let mut i = 0;
+
+    loop {
+        count += 1;
+
+        let j = i + jumps[i as usize];
+
+        if j >= 0 && j < jumps.len() as i64 {
+            if jumps[i as usize] >= 3 {
+                jumps[i as usize] -= 1;
+            } else {
+                jumps[i as usize] += 1;
+            }
+
+            i = j;
+        } else {
+            break;
+        }
+
+    }
+
+    count
+}
+
+fn main() {
+    println!("{}", solve(include_str!("5.in")));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::solve;
+
+    #[test]
+    fn examples() {
+        assert_eq!(solve("0\n3\n0\n1\n-3\n"), 10);
+    }
+}
