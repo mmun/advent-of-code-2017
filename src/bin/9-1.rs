6951de241d038300f600fd2bdd62856c69ff456b
Martin Muñoz <im.mmun@gmail.com>
2017-12-09 14:20:30 -0500

    Day 9

---
+++ src/bin/9-1.rs
@@ -0,0 +1,48 @@
+fn score(input: &str) -> i64 {
+    let mut chars = input.chars();
+
+    let mut depth = 0;
+    let mut score = 0;
+    let mut in_garbage = false;
+
+    while let Some(chr) = chars.next() {
+        if in_garbage {
+            match chr {
+                '>' => { in_garbage = false; },
+                '!' => { chars.next(); },
+                _ => {}
+            }
+        } else {
+            match chr {
+                '{' => { depth += 1; },
+                '}' => { depth -= 1; score += depth + 1; },
+                '<' => { in_garbage = true; },
+                _ => {}
+            }            
+        }
+    }
+
+    score
+}
+
+fn main() {
+    println!("{}", score(include_str!("9.in")));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::score;
+
+    #[test]
+    fn examples() {
+        assert_eq!(score("{}"), 1);
+        assert_eq!(score("{{{}}}"), 6);
+        assert_eq!(score("{{},{}}"), 5);
+        assert_eq!(score("{{{},{},{{}}}}"), 16);
+        assert_eq!(score("{<a>,<a>,<a>,<a>}"), 1);
+        assert_eq!(score("{{<ab>},{<ab>},{<ab>},{<ab>}}"), 9);
+        assert_eq!(score("{{<!!>},{<!!>},{<!!>},{<!!>}}"), 9);
+        assert_eq!(score("{{<a!>},{<a!>},{<a!>},{<ab>}}"), 3);
+    }
+}
+ 
---
+++ src/bin/9-2.rs
@@ -0,0 +1,44 @@
+fn score(input: &str) -> i64 {
+    let mut chars = input.chars();
+
+    let mut in_garbage = false;
+    let mut garbage_char_count = 0;
+
+    while let Some(chr) = chars.next() {
+        if in_garbage {
+            match chr {
+                '>' => { in_garbage = false; },
+                '!' => { chars.next(); },
+                _ => { garbage_char_count += 1; }
+            }
+        } else {
+            match chr {
+                '<' => { in_garbage = true; },
+                _ => {}
+            }            
+        }
+    }
+
+    garbage_char_count
+}
+
+fn main() {
+    println!("{}", score(include_str!("9.in")));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::score;
+
+    #[test]
+    fn examples() {
+        assert_eq!(score("<>"), 0);
+        assert_eq!(score("<random characters>"), 17);
+        assert_eq!(score("<<<<>"), 3);
+        assert_eq!(score("<{!>}>"), 2);
+        assert_eq!(score("<!!>"), 0);
+        assert_eq!(score("<!!!>>"), 0);
+        assert_eq!(score("<{o\"i!a,<{i<a>"), 10);
+    }
+}
+ 
