f867e4f537706b387c35248efb7e19f2497156c6
Martin Muñoz <im.mmun@gmail.com>
2017-12-12 09:26:14 -0500

    Day 12

---
+++ src/bin/12-1.rs
@@ -0,0 +1,61 @@
+type NodeIndex = usize;
+type Adj = Vec<Vec<NodeIndex>>;
+type Seen = Vec<bool>;
+
+fn solve(input: &str) -> i64 {
+    let mut adj: Adj = Vec::new();
+    let mut seen: Seen = Vec::new();
+
+    for line in input.trim().lines() {
+        let mut tokens = line.trim().split(" <-> ");
+
+        tokens.next();
+        
+        let children = tokens.next().unwrap()
+                .split(", ")
+                .map(|t| t.parse().unwrap())
+                .collect();
+
+        adj.push(children);
+        seen.push(false);
+    }
+
+    count(0, &adj, &mut seen)
+}
+
+fn count(i: NodeIndex, adj: &Adj, seen: &mut Seen) -> i64 {
+    seen[i] = true;
+
+    let mut sum = 1;
+
+    for &j in &adj[i] {
+        if !seen[j] {
+            sum += count(j, adj, seen);
+        }
+    }
+
+    sum
+}
+
+fn main() {
+    println!("{}", solve(include_str!("12.in")));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::solve;
+
+    #[test]
+    fn examples() {
+        assert_eq!(solve("
+            0 <-> 2
+            1 <-> 1
+            2 <-> 0, 3, 4
+            3 <-> 2, 4
+            4 <-> 2, 3, 6
+            5 <-> 6
+            6 <-> 4, 5
+        "), 6);
+    }
+}
+ 
---
+++ src/bin/12-2.rs
@@ -0,0 +1,66 @@
+type NodeIndex = usize;
+type Adj = Vec<Vec<NodeIndex>>;
+type Seen = Vec<bool>;
+
+fn solve(input: &str) -> i64 {
+    let mut adj: Adj = Vec::new();
+    let mut seen: Seen = Vec::new();
+
+    for line in input.trim().lines() {
+        let mut tokens = line.trim().split(" <-> ");
+
+        tokens.next();
+        
+        let children = tokens.next().unwrap()
+                .split(", ")
+                .map(|t| t.parse().unwrap())
+                .collect();
+
+        adj.push(children);
+        seen.push(false);
+    }
+
+    let mut count = 0;
+
+    for i in 0..adj.len() {
+        if !seen[i] {
+            count += 1;
+            visit(i, &adj, &mut seen);
+        }
+    }
+    
+    count
+}
+
+fn visit(i: NodeIndex, adj: &Adj, seen: &mut Seen) {
+    seen[i] = true;
+
+    for &j in &adj[i] {
+        if !seen[j] {
+            visit(j, adj, seen);
+        }
+    }
+}
+
+fn main() {
+    println!("{}", solve(include_str!("12.in")));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::solve;
+
+    #[test]
+    fn examples() {
+        assert_eq!(solve("
+            0 <-> 2
+            1 <-> 1
+            2 <-> 0, 3, 4
+            3 <-> 2, 4
+            4 <-> 2, 3, 6
+            5 <-> 6
+            6 <-> 4, 5
+        "), 2);
+    }
+}
+ 
