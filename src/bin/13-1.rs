d278ba2ae24abf769a96b679977b9189de2e50fd
Martin Muñoz <im.mmun@gmail.com>
2017-12-13 13:34:54 -0500

    Day 13

---
+++ src/bin/13-1.rs
@@ -0,0 +1,36 @@
+fn solve(input: &str) -> u64 {
+    let mut severity = 0;
+
+    for line in input.trim().lines() {
+        let mut tokens = line.trim().split(": ");
+
+        let depth = tokens.next().unwrap().parse::<u64>().unwrap();
+        let range = tokens.next().unwrap().parse::<u64>().unwrap();
+
+        if depth % (2*range - 2) == 0 {
+            severity += depth * range;
+        }
+    }
+
+    severity
+}
+
+fn main() {
+    println!("{}", solve(include_str!("13.in")));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::solve;
+
+    #[test]
+    fn examples() {
+        assert_eq!(solve("
+            0: 3
+            1: 2
+            4: 4
+            6: 4
+        "), 24);
+    }
+}
+ 
---
+++ src/bin/13-2.rs
@@ -0,0 +1,42 @@
+fn solve(input: &str) -> u64 {
+    let mut scanners = Vec::new();
+
+    for line in input.trim().lines() {
+        let mut tokens = line.trim().split(": ");
+        let depth = tokens.next().unwrap().parse::<u64>().unwrap();
+        let range = tokens.next().unwrap().parse::<u64>().unwrap();
+        scanners.push((depth, range));
+    }
+
+    'delay: for delay in 0.. {
+        for &(depth, range) in &scanners {
+            if (depth + delay) % (2*range - 2) == 0 {
+                continue 'delay;
+            }
+        }
+
+        return delay;
+    };
+
+    unreachable!();
+}
+
+fn main() {
+    println!("{}", solve(include_str!("13.in")));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::solve;
+
+    #[test]
+    fn examples() {
+        assert_eq!(solve("
+            0: 3
+            1: 2
+            4: 4
+            6: 4
+        "), 10);
+    }
+}
+ 
